1e68938bda0212395bdc03e189f79550f29c2210
Ksmarttechnica <kajal.smarttechnica@gmail.com>
2021-06-25 15:55:35 +0530

    change README.md file, and add record for seeder

--- README.md
+++ README.md
@@ -1,29 +1,25 @@
-# Laravel Pagination 
-
+# Laravel Pagination
 Laravel default pagination example with customize
-
-## Demo setup setps
-
+## Demo setup steps
+### Versions
+```base
+php version - "^7.3|^8.0"
+laravel version - "^8.40"
+```
 ### Clone
 ```base
 git clone git@github.com:hemalkachhadiya/Laravel-with-crud-and-pagination.git
 ```
-
-### System Requirements
-```base
-php version - "^7.3|^8.0"
-```
-
 ### Install composer
-Go to the project directory and install composer
-```base
+Go to the project directory   
+install composer using cmd
+```composer
 composer install
 ```
-
 ### setup .env file
+copy ```.env.example``` and paste in project and rename to ```.env```   
+first create database and setup credentials in ```.env``` file
 ```php
-copy .env.example and paste in project and rename to .env
-
 APP_URL=
 DB_CONNECTION=mysql
 DB_HOST=127.0.0.1
@@ -31,15 +27,16 @@ DB_PORT=3306
 DB_DATABASE=demo_laravel_pagination
 DB_USERNAME=root
 DB_PASSWORD=
-
-after env setup run cammand
-
+```
+after env setup run below commands for database
+```base
 php artisan key:generate
 php artisan optimize:clear
 php artisan migrate
+php artisan db:seed
 ```
-
 ### Install npm for tailwind css
+If you can't see css then run below commands
 ```base
 npm install
 npm run dev
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,16 +17,48 @@ class UserSeeder extends Seeder {
         User::insert( array(
             [
                 'name' => 'Smart Ravi',
-                'email' => 'ravi.smarttechnica@gmail.com',
-                'phone' => '9558688874',
+                'email' => 'ravi@gmail.com',
+                'phone' => '9558687474',
                 'password' => Hash::make( '123456789' ),
                 'password_base64' => base64_encode( '123456789' ),
                 'created_at' => NOW(),
                 'updated_at' => NOW()
             ], [
                 'name' => 'Smart Pankaj',
-                'email' => 'pankaj.smarttechnica@gmail.com',
-                'phone' => '9558688874',
+                'email' => 'pankaj@gmail.com',
+                'phone' => '9558685874',
+                'password' => Hash::make( '123456789' ),
+                'password_base64' => base64_encode( '123456789' ),
+                'created_at' => NOW(),
+                'updated_at' => NOW()
+            ], [
+                'name' => 'Smart Bhavik',
+                'email' => 'bhavik@gmail.com',
+                'phone' => '9559688874',
+                'password' => Hash::make( '123456789' ),
+                'password_base64' => base64_encode( '123456789' ),
+                'created_at' => NOW(),
+                'updated_at' => NOW()
+            ], [
+                'name' => 'Smart Dhaval',
+                'email' => 'dhaval@gmail.com',
+                'phone' => '9558568874',
+                'password' => Hash::make( '123456789' ),
+                'password_base64' => base64_encode( '123456789' ),
+                'created_at' => NOW(),
+                'updated_at' => NOW()
+            ], [
+                'name' => 'Smart Bhagirath',
+                'email' => 'bhagirath@gmail.com',
+                'phone' => '9572568874',
+                'password' => Hash::make( '123456789' ),
+                'password_base64' => base64_encode( '123456789' ),
+                'created_at' => NOW(),
+                'updated_at' => NOW()
+            ], [
+                'name' => 'Smart Keyur',
+                'email' => 'keyur@gmail.com',
+                'phone' => '9558568874',
                 'password' => Hash::make( '123456789' ),
                 'password_base64' => base64_encode( '123456789' ),
                 'created_at' => NOW(),
